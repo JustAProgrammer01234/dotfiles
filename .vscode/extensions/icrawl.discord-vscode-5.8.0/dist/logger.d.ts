4999782d751dd402bc75ba59605e33a9405b5083
JustAProgrammer01234 <76458223+JustAProgrammer01234@users.noreply.github.com>
2022-07-07 20:15:01 +0800

    idk what I'm doing with this shit but yeah.

--- .vscode/extensions/icrawl.discord-vscode-5.8.0/dist/logger.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const enum LogLevel {
-    Trace = "TRACE",
-    Debug = "DEBUG",
-    Info = "INFO",
-    Warn = "WARN",
-    Error = "ERROR"
-}
-export declare function log(level: LogLevel, message: string | Error): void;
